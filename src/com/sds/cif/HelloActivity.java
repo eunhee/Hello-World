2eb3f6b6c4eaec3804630c81ec16045fe5e2cb53
Eunhee Kim <lisa.exalt9@gmail.com>
2011-05-04 12:26:10 +0900

    New Project Hello-World
    Signed-off-by: Eunhee Kim <lisa.exalt9@gmail.com>

---
+++ src/com/sds/cif/HelloActivity.java
@@ -0,0 +1,13 @@
+package com.sds.cif;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
